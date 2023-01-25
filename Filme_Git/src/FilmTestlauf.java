700549c83e0203e76712eb856b72042c1a990133
CyricsNexus <68751459+CyricsNexus@users.noreply.github.com>
2023-01-25 23:35:39 +0100

    Update FilmTestlauf.java

--- Filme_Git/src/FilmTestlauf.java
+++ Filme_Git/src/FilmTestlauf.java
@@ -7,28 +7,30 @@ public class FilmTestlauf {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Wie viele Filme?");
+		System.out.println("Wie viele Filme? (max. 3)");
 		int eingabe = sc.nextInt();
 		
-		Filme [] meineFilme = new Filme[eingabe];
-		for (int i = 0; i<eingabe;i++) {
-			meineFilme[i] = new Filme();
+		if (eingabe > 3) {
+			Filme [] meineFilme = new Filme[eingabe];
+			for (int i = 0; i<eingabe;i++) {
+				meineFilme[i] = new Filme();
+			}
+
+			meineFilme[0].titel = "Vier Deadlines und ein Todesfall";
+			meineFilme[0].genre = "Drama";
+
+			meineFilme[1].titel = "Zwei Variablen trumpfen auf";
+			meineFilme[1].genre = "Komoedie";
+
+			meineFilme[2].titel = "Die Hoelle des gelben Koenigs";
+			meineFilme[2].genre = "Horror";
+
+			for (int i = 0; i<eingabe;i++) {
+				meineFilme[i].vorfuehren();
+			}
+		} else {
+			System.out.print("Das kann doch keiner an einem Abend sehen.");
 		}
-		
-		meineFilme[0].titel = "Vier Deadlines und ein Todesfall";
-		meineFilme[0].genre = "Drama";
-
-		meineFilme[1].titel = "Zwei Variablen trumpfen auf";
-		meineFilme[1].genre = "Komödie";
-		
-		meineFilme[2].titel = "Die Hölle des gelben Königs";
-		meineFilme[2].genre = "Horror";
-
-		for (int i = 0; i<eingabe;i++) {
-			meineFilme[i].vorführen();
-		}
-
-		
 		
 		sc.close();
 
